2a304419bf64991fb1a427705b0d42293c18fcd2
Furcht968 <tomu.iwanaga129+github@gmail.com>
2021-01-23 22:42:20 +0900

    add: dart simple package追加、description, README.md編集

---
+++ lib/MathDart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library MathDart;
+
+export 'src/MathDart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
